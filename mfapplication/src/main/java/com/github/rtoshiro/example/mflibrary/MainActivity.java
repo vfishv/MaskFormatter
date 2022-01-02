15ad06a6826937c304cbfaac1757b1a1e7684cec
vFISHv <vfishv@gmail.com>
2022-01-02 19:01:42 +0800

    androidx

--- mfapplication/src/main/java/com/github/rtoshiro/example/mflibrary/MainActivity.java
+++ mfapplication/src/main/java/com/github/rtoshiro/example/mflibrary/MainActivity.java
@@ -1,7 +1,7 @@
 package com.github.rtoshiro.example.mflibrary;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.EditText;
 
 import com.github.rtoshiro.util.format.MaskFormatter;
